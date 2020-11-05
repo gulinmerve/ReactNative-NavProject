525075a45c56212a94923b02cee0a2d13d2bcf73
Merve <gulineyel@gmail.com>
2020-11-05 22:30:07 +0300

    newspage

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
-import Router from './src-JobApp/Router';
+import Router from './src-myFavorite/Router';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => Router);
